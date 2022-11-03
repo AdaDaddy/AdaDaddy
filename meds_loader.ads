d4aad160c923dbf29009dc1c1a365f2b9626a96a
AdaDaddy <112723411+AdaDaddy@users.noreply.github.com>
2022-11-03 10:33:27 -0600

    Add files via upload
    New formatting changes for the output files.  Added the ability to exclude resident names when a patient has recently left the facility or died but is still in the streamlined_MAR file.

--- meds_loader.ads
+++ meds_loader.ads
@@ -26,11 +26,17 @@ package Meds_Loader is
    -- data structure
    procedure read_text_medlist (Medlist : out resident_meds; From : String);
 
+   -- filter out resident(s) by names listed in the file
+   -- designated by the From parameter;
+   procedure filter_medlist (Medlist : in out resident_meds; from : String);
+
    -- Output the resident_meds data in human-readable format
-   procedure display_medlist (Medlist : resident_meds);
+   procedure display_medlist
+     (Medlist : resident_meds; filename : String; Facility : String);
 
    -- Output the resident_meds data in JSON format
-   procedure write_medlist (Medlist : resident_meds; filename : String);
+   procedure write_medlist
+     (Medlist : resident_meds; filename : String; Facility : String);
 
 private
 
@@ -39,24 +45,40 @@ private
       Brand_Names  : Unbounded_String;
       Generic_Name : Unbounded_String;
       Instructions : Unbounded_String;
-      Hours        : Unbounded_String;
    end record;
 
    package med_vector is new Ada.Containers.Vectors
      (Index_Type => Positive, Element_Type => Med_Data);
    use med_vector;
 
-   -- structure of a medication record for a single resident
+   -- type to index the array of meds according to their
+   -- administration period
+   type Med_Admin_Period is (Morning, Noon, Afternoon, Night, PRN);
+
+   -- Array type to hold vectors of med_data for each administration
+   -- period
+   type Meds_Lists is array (Med_Admin_Period) of med_vector.Vector;
+
+   -- structure of a resident_info record for a single resident
+   -- The meds field is an array of med_vector.Vector so that the
+   -- meds for each resident can be listed for each administration period
    type resident_info is record
-      Name         : Unbounded_String;
-      Birthdate     : Unbounded_String;
-      Diagnosis    : Unbounded_String;
-      Allergies    : Unbounded_String;
-      Sex          : Unbounded_String;
-      Meds_Comment : Unbounded_String;
-      Meds         : med_vector.Vector := med_vector.Empty_Vector;
+      Id             : Unbounded_String := Null_Unbounded_String;
+      Room_Number    : Unbounded_String := Null_Unbounded_String;
+      Photo          : Unbounded_String := Null_Unbounded_String;
+      Contact_Name   : Unbounded_String := Null_Unbounded_String;
+      Contact_Number : Unbounded_String := Null_Unbounded_String;
+      Contact_Email  : Unbounded_String := Null_Unbounded_String;
+      Name           : Unbounded_String := Null_Unbounded_String;
+      Birthdate      : Unbounded_String := Null_Unbounded_String;
+      Diagnosis      : Unbounded_String := Null_Unbounded_String;
+      Allergies      : Unbounded_String := Null_Unbounded_String;
+      Sex            : Unbounded_String := Null_Unbounded_String;
+      Meds_Comment   : Unbounded_String := Null_Unbounded_String;
+      Meds           : Meds_Lists       := (others => med_vector.Empty_Vector);
    end record;
 
+   -- instantiate Ada.Containers.Vectors for the resident_info type
    package resident_vector is new Ada.Containers.Vectors
      (Index_Type => Positive, Element_Type => resident_info);
    use resident_vector;
